536287ed8b3c9a2ad68d4fb12f8a14dd7898e36c
Hasan Malik <hasanimranmalik245@gmail.com>
2026-02-17 09:03:37 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import LandingPage from "./pages/LandingPage"
+
+
+export default function App() {
+  // const [isAuthed, setIsAuthed] = useState(false)
+
+  // return isAuthed ? (
+  //   <FeedPage onLogout={() => setIsAuthed(false)} />
+  // ) : (
+  //   <LandingPage
+  //     onLoginClick={() => setIsAuthed(true)}
+  //     onSignupClick={() => setIsAuthed(true)}
+  //   />
+  // )
+
+  return <LandingPage
+      onLoginClick={() => setIsAuthed(true)}
+      onSignupClick={() => setIsAuthed(true)}
+    />
+}
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { MessageCircle, Shield, Sparkles, Users } from "lucide-react"
+
+
+// reusable Feature component:
+const Feature = ({icon, title, desc}) => {
+    const Icon = icon;
+
+    return (
+        <div className="flex gap-3 rounded-xl border bg-card p-4">
+            <div className="mt-0.5 inline-flex h-10 w-10 
+            justify-center items-center
+            rounded-lg border bg-background">
+                <Icon className='h-5 w-5'></Icon>
+            </div>
+
+            <div>
+                <div className="font-semibold leading-tight">{title}</div>
+                <div className="text-sm text-muted-foreground">{desc}</div>
+            </div>
+        </div>
+    )
+}
+
+export default function LandingPage() {
+  return (
+    <>
+        
+
+    </>
+  )
+}
